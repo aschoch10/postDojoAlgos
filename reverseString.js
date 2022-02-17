8402f35d35dd5316350a90d3ca0bddcf40672f43
Andrew <aschoch10@gmail.com>
2022-02-17 08:57:17 -0800

    initial commit

---
+++ reverseString.js
@@ -0,0 +1,17 @@
+// Write a function that will reverse the values an array and return them.
+
+function reverse(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    while(left < right) {
+        var temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+    return arr;
+}
+
+let result = reverse(["a", "b", "c", "d", "e"]);
+console.log(result); // we expect back ["e", "d", "c", "b", "a"]

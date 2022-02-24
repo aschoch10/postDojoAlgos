2b3f7f03cc756540d8cc53ed811c155b1f265ee9
Andrew <aschoch10@gmail.com>
2022-02-23 16:11:40 -0800

    tested string

--- isPalindrome.js
+++ isPalindrome.js
@@ -13,14 +13,16 @@ let isPalindrome = (x) => {
             return false
         }
         
-}
+    }
 };
 
 x1 = 121
 x2 = 1234
 x3 = -121
 x4 = 1221
+x5 = "abasa"
 console.log(isPalindrome(x1));
 console.log(isPalindrome(x2));
 console.log(isPalindrome(x3));
 console.log(isPalindrome(x4));
+console.log(isPalindrome(x5));
